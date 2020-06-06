115b1deb5b7516df91f15e5db30d3a0592a6b6af
OmarAliSoliman <49602133+OmarAliSoliman@users.noreply.github.com>
2020-06-06 17:55:16 +0200

    ft

--- js/adspage.js
+++ js/adspage.js
@@ -1,27 +1,20 @@
 $(document).ready(function(){
 
-  var galleryThumbs = new Swiper('.gallery-thumbs', {
-    spaceBetween: 10,
-    slidesPerView: 4,
-    loop: true,
-    freeMode: true,
-    loopedSlides: 5, //looped slides should be the same
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
+  $('.slider-top').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    dots: false,
+    asNavFor: '.slider-bottom'
   });
-  var galleryTop = new Swiper('.gallery-top', {
-    spaceBetween: 10,
-    loop:true,
-    loopedSlides: 5, //looped slides should be the same
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    thumbs: {
-      swiper: galleryThumbs,
-    },
+  $('.slider-bottom').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.slider-top',
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true
   });
-
-
 
 })
